60cb0756a1ffd2a2e9333f41aaa31ea5bce6cc26
Ruslan Kolomenskiy <charly100@yandex.ru>
2024-04-24 13:23:19 +0300

    SnakeGame

---
+++ SnakeGame/Source/SnakeGame/SnakeBase.cpp
@@ -0,0 +1,94 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SnakeBase.h"
+#include "SnakeElementBase.h"
+
+// Sets default values
+ASnakeBase::ASnakeBase()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	ElementSize = 100.f;
+	LastMoveDirection = EMovementDirection::DOWN;
+
+}
+
+// Called when the game starts or when spawned
+void ASnakeBase::BeginPlay()
+{
+	Super::BeginPlay();
+	SetActorTickInterval(MovementSpeed);
+	AddSnakeElement(4);
+	
+}
+
+// Called every frame
+void ASnakeBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	Move();
+
+}
+
+void ASnakeBase::AddSnakeElement(int ElementsNum)
+{
+	for (int i = 0; i < ElementsNum; ++i)
+	{
+
+
+		FVector NewLocation(SnakeElements.Num() * ElementSize, 0, 0);
+		FTransform NewTransform(NewLocation);
+		ASnakeElementBase* NeSnakeElem = GetWorld()->SpawnActor<ASnakeElementBase>(SnakeElementClass, NewTransform);
+		int32 ElemIndex = SnakeElements.Add(NeSnakeElem);
+		if (ElemIndex == 0)
+		{
+			NeSnakeElem->SetFirstElementType();
+		}
+	}
+		
+}
+
+void ASnakeBase::Move()
+{
+	FVector MovementVector(ForceInitToZero);
+     MovementSpeed = ElementSize;
+
+	switch (LastMoveDirection)
+	{
+	case EMovementDirection::UP:
+		MovementVector.X += MovementSpeed;
+		break;
+	case EMovementDirection::DOWN:
+		MovementVector.X -= MovementSpeed;
+		break;
+	case EMovementDirection::LEFT:
+		MovementVector.Y -= MovementSpeed;
+		break;
+	case EMovementDirection::RIGHT:
+		MovementVector.Y += MovementSpeed;
+		break;
+
+	}
+	
+	auto HeadElement = SnakeElements[0];
+	FVector HeadLocation = HeadElement->GetActorLocation();
+	SnakeElements[0]->AddActorWorldOffset(MovementVector);
+
+	
+	for (int i = 1; i < SnakeElements.Num(); i++)
+	{
+		auto NextElement = SnakeElements[i];
+		FVector NextElementLocation = NextElement->GetActorLocation();
+		NextElement->SetActorLocation(HeadLocation);
+		HeadLocation = NextElementLocation;
+
+
+	}
+	
+	
+	
+
+	
+	
+}
